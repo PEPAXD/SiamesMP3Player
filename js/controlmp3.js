b39d6b377f8169ab780426b655e95892d1a8c2f8
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-10-16 03:01:16 -0300

    AddArraysSongs

--- js/controlmp3.js
+++ js/controlmp3.js
@@ -1,15 +1,65 @@
-// PLAY / PAUSE SONG
-var mysong = document.getElementById("mysong");
-var toggleButton = document.getElementById("playButton");
-var icon = toggleButton.querySelector("i");
 
-toggleButton.addEventListener("click", function () {
+//getID
+const mysong = document.getElementById("mysong");
+const playButton = document.getElementById("playButton");
+const nextButton = document.getElementById("nextButton");
+const prevButton = document.getElementById("prevButton");
+const songTitleElement = document.getElementById("songTitle");
+const songAlbumElement = document.getElementById("songAlbum");
+const artSong = document.querySelector(".album-art");
 
-    if (mysong.paused) {
-        mysong.play();
-        icon.className = "fa-solid fa-pause";
-    } else {
+// Manejadores de eventos para los botones
+playButton.addEventListener("click", togglePlayPause);
+nextButton.addEventListener("click", playNextSong);
+prevButton.addEventListener("click", playPrevSong);
+
+
+// Variables para rastrear el estado de reproducción
+let isPlaying = false;
+let currentSongIndex = 0;
+
+loadSong(currentSongIndex);
+
+// PAUSE / PLAY
+function togglePlayPause() {
+    if (isPlaying) {
         mysong.pause();
-        icon.className = "fa-solid fa-play";
+        playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+    } else {
+        mysong.play();
+        playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
     }
-});
+    isPlaying = !isPlaying;
+}
+
+// NEXT
+function playNextSong() {
+    currentSongIndex = (currentSongIndex + 1) % allMusic.length;
+    loadSong(currentSongIndex);
+    mysong.play();
+
+}
+
+// PREV
+function playPrevSong() {
+    currentSongIndex = (currentSongIndex - 1 + allMusic.length) % allMusic.length;
+    loadSong(currentSongIndex);
+    mysong.play();
+}
+
+// LoadSong
+function loadSong(index) {
+    const song = allMusic[index];
+    mysong.src = `songs/${song.src}.mp3`;
+    playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+    isPlaying = false;
+    console.log(`Play: ${song.name}`);
+
+    //update DataSong
+    songTitleElement.innerHTML = `<span><i class="fa-solid fa-music" style="color: #ffffff;"></i></span>${song.name}`;
+    songAlbumElement.innerHTML = `<span><i class="fa-solid fa-compact-disc" style="color: #ffffff;"></i></span>${song.disc}`;
+    artSong.style.backgroundImage = `url('DiscArt/${song.art}')`;
+
+}
+
+
---
+++ js/musicList.js
@@ -0,0 +1,20 @@
+let allMusic = [
+{
+    name: "the wolf",
+    art:  "theWolf.jpg",
+    disc: "bounce into the music",
+    src: "TheWolf"
+},
+{
+    name: "b.i.t.m",
+    art:  "BITM.svg",
+    disc: "bounce into the music",
+    src: "BITM"
+},
+{
+    name: "mr fear",
+    art:  "MrFear.jpg",
+    disc: "bounce into the music",
+    src: "MrFear"
+}
+];
